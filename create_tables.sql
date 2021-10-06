8442beb99d58e79a270e6503cf5ad71d689e414f
max <harumaxy@gmail.com>
2021-10-06 09:50:07 +0900

    update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE public.rooms (
-  id serial NOT NULL UNIQUE,
+  id serial NOT NULL PRIMARY KEY,
   name text NOT NULL,
   created_at timestamp without time zone NOT NULL DEFAULT now()
 );
 
 
 CREATE TABLE public.chat_messages (
-  id serial NOT NULL UNIQUE,
+  id serial NOT NULL PRIMARY KEY,
   room_id integer NOT NULL REFERENCES public.rooms(id),
   user_id uuid NOT NULL,
   message text NOT NULL,
